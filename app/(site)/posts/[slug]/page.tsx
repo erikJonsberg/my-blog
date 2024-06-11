48166ffff73457739c1a629ddb6d97378d6444e8
Erik Jonsberg <erikj@hungryghostdesign.com>
2024-06-11 11:59:13 -0400

    forced static on single posts

--- app/(site)/posts/[slug]/page.tsx
+++ app/(site)/posts/[slug]/page.tsx
@@ -1,10 +1,10 @@
 import { QueryParams, SanityDocument } from 'next-sanity';
-import { draftMode } from 'next/headers';
 import { loadQuery } from '@/sanity/lib/store';
 import { POSTS_QUERY, POST_QUERY } from '@/sanity/lib/queries';
 import Post from '@/app/components/posts/single-post';
-import PostPreview from '@/app/components/posts/preview-post';
 import { client } from '@/sanity/lib/client';
+
+export const dynamic = 'force-static';
 
 export async function generateStaticParams() {
 	const posts = await client.fetch<SanityDocument[]>(POSTS_QUERY);
@@ -15,20 +15,9 @@ export async function generateStaticParams() {
 }
 
 export default async function Page({ params }: { params: QueryParams }) {
-	const initial = await loadQuery<SanityDocument>(POST_QUERY, params, {
-		// Because of Next.js, RSC and Dynamic Routes this currently
-		// cannot be set on the loadQuery function at the "top level"
-		perspective: draftMode().isEnabled ? 'previewDrafts' : 'published',
-	});
+	const { slug } = params;
 
-	console.log(initial.data);
+	const post = await loadQuery<SanityDocument>(POST_QUERY, { slug });
 
-	return draftMode().isEnabled ? (
-		<PostPreview
-			initial={initial}
-			params={params}
-		/>
-	) : (
-		<Post post={initial.data} />
-	);
+	return <Post post={post.data} />;
 }
